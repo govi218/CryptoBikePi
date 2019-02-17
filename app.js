bbb282fc7bc2f7e49ddc252a5636f3e7a47f978f
gov <govi218mu@gmail.com>
2019-02-17 04:33:04 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const azure = req
+
+/** Init **/
+
+let app = express();
+
+const port = 3000;
+
+// express config
+
+// CORS policy
+let allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
+
+app
+  .use(allowCrossDomain)
+  .use(bodyParser.urlencoded());
+
+/** File Serving Routes **/
+
+app.post('/state_change', (req, res) => {
+  if (!exists(req.body)) {
+    res
+      .status(400)
+      .send('Error: Invalid request');
+      return;
+  }
+  console.log('State changed:');
+  console.log(req.body);
+  res.status(200);
+});
+
+/* Server Config */
+app.listen(port, () => {
+  console.log('listening on port ' + port);
+});
+
+/* Helpers */
+
+// Checks if an object exists
+let exists = (obj) => {
+  if (obj === null || obj === undefined || obj === '') {
+    return false;
+  }
+  return true;
+}
